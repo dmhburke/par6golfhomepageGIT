bb6ee7bb39719dee52c9073947a3005154298f2a
Daniel Burke <dmhburke@gmail.com>
2019-12-02 18:30:10 -0500

    Fix views issue

--- catalog/views.py
+++ catalog/views.py
@@ -159,21 +159,21 @@ def landingpage(request):
         snakepit_winner = ""
 # === 1. TOUR OF ORIGIN (Tampa, Florida) ===
     # TOUR ADMIN - Origin
-        origin = TourModel.objects.get(title="The Tour of Origin").title
-        origin_location = TourModel.objects.get(title=origin).location
-        origin_number = TourModel.objects.get(title=origin).tour_number
-        origin_dates = TourModel.objects.get(title=origin).tour_dates
+    origin = TourModel.objects.get(title="The Tour of Origin").title
+    origin_location = TourModel.objects.get(title=origin).location
+    origin_number = TourModel.objects.get(title=origin).tour_number
+    origin_dates = TourModel.objects.get(title=origin).tour_dates
 
     # TOUR DETAILS - Origin
-        origin_players = TourPlayerModel.objects.filter(tour_title__title=origin).order_by('tour_playernumber')
-        origin_courses = TourCoursesModel.objects.filter(tour_title__title=origin)
-        origin_result = TourPlayerModel.objects.filter(tour_title__title=origin, tour_position__lte=6).order_by('tour_position')
-        origin_organizer = TourPlayerModel.objects.filter(tour_title__title=origin, tour_organizer="Yes")
-        origin_url = ""
-        try:
-            origin_winner = TourPlayerModel.objects.get(tour_title__title=origin, tour_position__lte=1).tour_player
-        except:
-            origin_winner = ""
+    origin_players = TourPlayerModel.objects.filter(tour_title__title=origin).order_by('tour_playernumber')
+    origin_courses = TourCoursesModel.objects.filter(tour_title__title=origin)
+    origin_result = TourPlayerModel.objects.filter(tour_title__title=origin, tour_position__lte=6).order_by('tour_position')
+    origin_organizer = TourPlayerModel.objects.filter(tour_title__title=origin, tour_organizer="Yes")
+    origin_url = ""
+    try:
+        origin_winner = TourPlayerModel.objects.get(tour_title__title=origin, tour_position__lte=1).tour_player
+    except:
+        origin_winner = ""
 
 
     context = {
